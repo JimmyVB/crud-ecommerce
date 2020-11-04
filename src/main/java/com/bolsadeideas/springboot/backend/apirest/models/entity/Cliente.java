17b674752957f3ddf199f1ccbb01db3692350468
Jimmy Valdez <jimmyvb16@gmail.com>
2020-11-03 23:50:12 -0500

    CRUD Cliente
    CRUD Cliente - Spring Framework 5 for back and Angular 10 for front

---
+++ src/main/java/com/bolsadeideas/springboot/backend/apirest/models/entity/Cliente.java
@@ -0,0 +1,104 @@
+package com.bolsadeideas.springboot.backend.apirest.models.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.io.Serializable;
+import java.util.Date;
+
+@Entity
+@Table(name = "clientes")
+public class Cliente implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotEmpty
+    @Size(min=4, max=12)
+    @Column(nullable = false)
+    private String nombre;
+
+    @NotEmpty
+    private String apellido;
+
+    @NotEmpty
+    @Column(nullable = false, unique = false)
+    @Email
+    private String email;
+
+    @NotNull
+    @Column(name = "create_at")
+    @Temporal(TemporalType.DATE)
+    private Date createAt;
+
+    private String foto;
+
+    @NotNull(message = "La region no puede ser vacia")
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="region_id")
+    @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+    private Region region;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getCreateAt() {
+        return createAt;
+    }
+
+    public void setCreateAt(Date createAt) {
+        this.createAt = createAt;
+    }
+
+    public String getFoto() {
+        return foto;
+    }
+
+    public void setFoto(String foto) {
+        this.foto = foto;
+    }
+
+    public Region getRegion() {
+        return region;
+    }
+
+    public void setRegion(Region region) {
+        this.region = region;
+    }
+
+    private static final long serialVersionUID = 2189314171408493743L;
+}
---
+++ src/main/resources/import.sql
@@ -0,0 +1,24 @@
+INSERT INTO regiones (id, nombre) VALUES (1, 'Sudamerica');
+INSERT INTO regiones (id, nombre) VALUES (2, 'Centroamerica');
+INSERT INTO regiones (id, nombre) VALUES (3, 'Norteamerica');
+INSERT INTO regiones (id, nombre) VALUES (4, 'Europa');
+INSERT INTO regiones (id, nombre) VALUES (5, 'Asia');
+INSERT INTO regiones (id, nombre) VALUES (6, 'Africa');
+INSERT INTO regiones (id, nombre) VALUES (7, 'Oceania');
+INSERT INTO regiones (id, nombre) VALUES (8, 'Antartida');
+
+INSERT INTO clientes (region_id, nombre, apellido, email, create_at) VALUES (1, 'Jimmy', 'Valdez', 'jimmy@gmail.com', '2020-01-01');
+INSERT INTO clientes (region_id, nombre, apellido, email, create_at) VALUES (2, 'Richard', 'Blas1', 'richard1@gmail.com', '2020-02-02');
+INSERT INTO clientes (region_id, nombre, apellido, email, create_at) VALUES (4, 'Antonio', 'Blas2', 'richar2d@gmail.com', '2020-02-02');
+INSERT INTO clientes (region_id, nombre, apellido, email, create_at) VALUES (4, 'Manuel', 'Blas3', 'richard3@gmail.com', '2020-02-02');
+INSERT INTO clientes (region_id, nombre, apellido, email, create_at) VALUES (4, 'Francisco', 'Blas4', 'richa4rd@gmail.com', '2020-02-02');
+INSERT INTO clientes (region_id, nombre, apellido, email, create_at) VALUES (3, 'David', 'Blas5', 'richar5d@gmail.com', '2020-02-02');
+INSERT INTO clientes (region_id, nombre, apellido, email, create_at) VALUES (3, 'Juan', 'Blas6', 'richard6@gmail.com', '2020-02-02');
+INSERT INTO clientes (region_id, nombre, apellido, email, create_at) VALUES (3, 'Javier', 'Blas7', 'richard7@gmail.com', '2020-02-02');
+INSERT INTO clientes (region_id, nombre, apellido, email, create_at) VALUES (3, 'Daniel', 'Blas8', 'richard8@gmail.com', '2020-02-02');
+INSERT INTO clientes (region_id, nombre, apellido, email, create_at) VALUES (5, 'Carmen', 'Blas9', 'richard9@gmail.com', '2020-02-02');
+INSERT INTO clientes (region_id, nombre, apellido, email, create_at) VALUES (6, 'Ana', 'Blas10', 'richar10d@gmail.com', '2020-02-02');
+INSERT INTO clientes (region_id, nombre, apellido, email, create_at) VALUES (7, 'Josefa', 'Blas11', 'richa11rd@gmail.com', '2020-02-02');
+INSERT INTO clientes (region_id, nombre, apellido, email, create_at) VALUES (7, 'Eloy', 'Blas12', 'richard12@gmail.com', '2020-02-02');
+INSERT INTO clientes (region_id, nombre, apellido, email, create_at) VALUES (7, 'Eduardo', 'Blas13', 'richar13d@gmail.com', '2020-02-02');
+INSERT INTO clientes (region_id, nombre, apellido, email, create_at) VALUES (8, 'Enrique', 'Blas14', 'richard14@gmail.com', '2020-02-02');
